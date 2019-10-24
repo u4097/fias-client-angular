cc4e605e9fbbc162efd0a14cc29ea683bd18fbd9
Oleg Sitnikov <oleg@home.local>
2019-10-24 21:11:39 +0300

    Add generation code for graphql  with graphql-codegen

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,77 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import gql from 'graphql-tag';
+import {UserByIdGQL} from "../generated/graphql";
 
 type User = {
-	  id: number;
-	  firstname: string;
-	  age: number;
+    id: string;
+    firstname: string;
+    age: number;
 }
 
 type Query = {
     users: User[];
-    user(id:number):User;
-    }
+    user(id: number): User;
+}
 
 type Variables = {
     id: number;
 }
 
 type Response = {
-	user: User;
+    user: User;
 }
 
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  users: Observable<User[]>;
-  user:User;
+    users: Observable<User[]>;
+    user: User;
 
-  constructor(private apollo: Apollo) {
-   
- }
+    constructor(private apollo: Apollo,
+                private userGQL: UserByIdGQL
+    ) {
 
-ngOnInit() {
-	    this.users = this.apollo.watchQuery<Query>({
-		          query: gql`
-			          query users {
-				   users {
-				   id
-				   firstname
-			           age 
-			      }
-		      }
-		    `
-		})
-.valueChanges
-	    .pipe( map(result => result.data.users));
+    }
+
+    ngOnInit() {
+        this.users = this.apollo.watchQuery<Query>({
+            query: gql`
+                query users {
+                    users {
+                        id
+                        firstname
+                        age
+                    }
+                }
+            `
+        })
+        .valueChanges
+        .pipe(map(result => result.data.users));
 
 
-	     this.apollo.watchQuery<Response, Variables>({
-		          query: gql`
-			           query user($id: ID!){
-				   user(id:$id) {
-				   id
-				   firstname
-			           age 
-			      }
-		      }
-		    `,
-		    variables: {
-			    id:1,
-		    },
-		})
-		.valueChanges.subscribe(({data}) => {
-		this.user = data.user;
-		console.log(this.user.firstname);
-	      });
-}
+        /*
+                 this.apollo.watchQuery<Response, Variables>({
+                          query: gql`
+                               query user($id: ID!){
+                           user(id:$id) {
+                           id
+                           firstname
+                               age
+                          }
+                      }
+                    `,
+                    variables: {
+                        id:1,
+                    },
+                })
+                .valueChanges.subscribe(({data}) => {
+                this.user = data.user;
+                console.log(this.user.firstname);
+                  });
+        */
+
+        let usr;
+        this.userGQL.watch({id: "3"})
+            .valueChanges
+            .subscribe(({data}) => {
+            this.user = data.user;
+            console.log(this.user.firstname);
+        });
+
+    }
 
 }
